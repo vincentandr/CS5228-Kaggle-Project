ee9a89113388f67f70569b6ba0e32ec3079afeff
jon-chua <jonchua97@gmail.com>
2021-04-11 14:52:11 +0800

    Add param grids and random forest results

--- models/param_grids/ada_boost_param_grid.py
+++ models/param_grids/ada_boost_param_grid.py
@@ -1,5 +1,9 @@
 import numpy as np
+from sklearn.tree import DecisionTreeRegressor
 
-param_grid = {"n_neighbors" : np.arange(1,11),
-              "weights"     : ['uniform', 'distance'],
-              "p"           : [1,2,4,8,16]}
+param_grid = {
+	"base_estimator"	: [DecisionTreeRegressor(max_depth=5), DecisionTreeRegressor(max_depth=7), DecisionTreeRegressor(max_depth=10)],
+	"n_estimators" 		: [200, 500, 1000],
+	"learning_rate"     : [1.0],
+	"random_state"		: [42],
+}
--- models/param_grids/random_forest_param_grid.py
+++ models/param_grids/random_forest_param_grid.py
@@ -1,4 +1,8 @@
 import numpy as np
 
-param_grid = {"n_estimators" : [10, 10, 100, 1000, 10000],
-    "max_depth"           : [100, 1000, 10000]}
+param_grid = {
+	"n_estimators" : [200, 500, 1000],
+    "random_state" : [42],
+    "verbose" 	   : [1],
+    "n_jobs"	   : [4],
+}
