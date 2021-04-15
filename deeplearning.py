7f7fc5290fdeee17727ae3faad8dffaff52f507b
Yosua Muliawan <yosua.muliawan@shopee.com>
2021-04-15 20:38:09 +0800

    added deeplearning models

--- deeplearning.py
+++ deeplearning.py
@@ -8,6 +8,9 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
+from keras import backend as K
+import numpy as np
+
 # load dataset
 # dataframe = read_csv("housing.csv", delim_whitespace=True, header=None)
 # dataset = dataframe.values
@@ -24,15 +27,22 @@ Y = data['resale_price'].values
 X = data.drop('resale_price', axis=1).values
 
 
+def root_mean_squared_error(y_true, y_pred):
+    return K.sqrt(K.mean(K.square(y_pred - y_true)))
+
 # define wider model
+
+
 def wider_model():
     # create model
     model = Sequential()
-    model.add(Dense(20, input_dim=98,
-              kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1, kernel_initializer='normal'))
-    # Compile model
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.add(Dense(101, input_dim=98, activation="relu"))
+    model.add(Dense(60, activation="relu"))
+    model.add(Dense(1))
+    model.summary()  # Print model Summary
+    # compile
+    model.compile(loss=root_mean_squared_error, optimizer="adam",
+                  metrics=[root_mean_squared_error, "mean_squared_error"])
     return model
 
 
@@ -40,8 +50,28 @@ def wider_model():
 estimators = []
 estimators.append(('standardize', StandardScaler()))
 estimators.append(('mlp', KerasRegressor(
-    build_fn=wider_model, epochs=100, batch_size=5, verbose=1)))
+    build_fn=wider_model, epochs=100, batch_size=10, verbose=1)))
 pipeline = Pipeline(estimators)
-kfold = KFold(n_splits=3)
-results = cross_val_score(pipeline, X, Y, cv=kfold)
-print("Wider: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+# kfold = KFold(n_splits=3)
+# results = cross_val_score(pipeline, X, Y, cv=kfold)
+# print("Wider: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+TEST_DATA = "./features/extracted_data/test_preprocessing_output.csv"
+PREDICTION_OUTPUT = "./results_analysis/results/experiment_output.csv"
+
+test_data = pd.read_csv(TEST_DATA)
+
+test_data['block'] = test_data['block'].str.replace(
+    '[A-Za-z]', '', regex=True)
+test_data['block'] = test_data['block'].apply(int)
+
+# training the model using best parameters
+pipeline.fit(X, Y)
+
+
+# actual prediction using model
+test_predictions = pipeline.predict(test_data)
+
+# saving data to csv
+result = pd.DataFrame(
+    {'Id': np.arange(len(test_predictions)), 'Predicted': test_predictions})
+result.to_csv(PREDICTION_OUTPUT, index=False)
